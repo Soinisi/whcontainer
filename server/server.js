0fc6f4eb029443bb1f1e8e4db1f3b056d8e13389
Simo Soininen <soinisi@gmail.com>
2021-01-05 21:21:57 +0200

    first commit, server docker up

---
+++ server/server.js
@@ -0,0 +1,31 @@
+
+// server.js
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+// Our DB Configuration
+require('./src/database');
+
+const bodyParser = require('body-parser');
+
+// Routes
+const postRouter = require('./src/routes/post.router');
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(bodyParser.json());
+
+app.use('/posts', postRouter);
+
+
+app.get('/', (req, res) => {
+    res.send("Hello World ! ");
+});
+
+app.listen(PORT, function () {
+    console.log(`Server Listening on ${PORT}`);
+});
